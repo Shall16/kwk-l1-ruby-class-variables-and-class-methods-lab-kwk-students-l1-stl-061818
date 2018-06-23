539e8227c0e818a6e3c32c92bfcabeb7839adad0
Shall16 <shall130@rsdmo.org>
2018-06-23 14:59:05 +0000

    automatically backed up by learn

--- album.rb
+++ album.rb
@@ -27,7 +27,5 @@ drake= Pinapples.new("Sorpion","2018")
 
 puts "The Album was made in #{shawn_mendes.date}"
 puts "The Album was made in #{taylor_swift.date}"
-puts "The Album, #{thebeetels.name_of_album} was made in #{thebeetels.date}" 
-puts "The Album, #{lin_manuel_miranda.name_of_album} was made in #{lin_manuel_miranda.date"
-
- 
+puts "The Album, #{thebeetels.name_of_album} was made in #{thebeetels.date}"
+puts "The Album, #{lin_manuel_miranda.name_of_album} was made in #{lin_manuel_miranda.date}" 
